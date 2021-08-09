a3db5c328f422fbfd1eb177889474455830af428
shubhojitnag <shubhojitnag2009@gmail.com>
2021-08-09 12:41:52 +0530

    sakura:Lets bring in the Crdroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bliss_sakura.mk
+ $(LOCAL_DIR)/lineage_sakura.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_sakura-user \
+    lineage_sakura-userdebug \
+    lineage_sakura-eng
