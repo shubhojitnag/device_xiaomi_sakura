d42e943bea4ddb4df0eeb7d151b3ce9db96f5ed5
shubhojitnag <shubhojitnag2009@gmail.com>
2021-08-09 12:41:52 +0530

    sakura:Added lineage stuffs

--- lineage_sakura.mk
+++ lineage_sakura.mk
@@ -18,15 +18,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit from mido device
+# Inherit from sakura device
 $(call inherit-product, device/xiaomi/sakura/device.mk)
 
-# Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sakura
-PRODUCT_NAME := bliss_sakura
+PRODUCT_NAME := lineage_sakura
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 6 Pro
