0bca864f6922ccf6e7d46a3d229f9148764f6192
shubhojitnag <shubhojitnag2009@gmail.com>
2021-09-08 21:20:45 +0530

    Sakura : Enable zram-writeback job This patch enables zram-writeback job. It will run idle marking at 60m since the device is boot. After that, it will do first writeback after 3hour to sweep initiallized cold pages out. After that, it periodically write out every 24hour.
    Test: confirm with manual testing
    Change-Id: I6857c79ca3ce6a3619f4b7e200d7f17a8decd659
    Signed-off-by: Randall Huang <huangrandall@google.com>

--- prop.mk
+++ prop.mk
@@ -232,3 +232,8 @@ persist.device_config.runtime_native.usap_pool_enabled=true
 #Blur
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.sys.sf.disable_blurs=1
+
+# ZRAM
+ro.zram.mark_idle_delay_mins=60
+ro.zram.first_wb_delay_mins=180
+ro.zram.periodic_wb_delay_hours=24
